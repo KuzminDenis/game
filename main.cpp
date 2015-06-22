f19ce499ee34df36c65cfadc748f3178287899e3
Denis <kuzmindenis94@gmail.com>
2015-06-22 23:09:11 +0300

    Some refactoring

--- main.cpp
+++ main.cpp
@@ -1,10 +1,47 @@
 #include <SDL2/SDL.h>
 #include "stdio.h"
+#include <string>
+
+using namespace std;
+
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
+SDL_Window *window = NULL;
+SDL_Renderer *renderer = NULL;
+
+SDL_Texture* LoadImage(string file)
+{
+    SDL_Surface *loadedImage = NULL;
+    SDL_Texture *texture = NULL;
+    loadedImage = SDL_LoadBMP(file.c_str());
+    if (loadedImage != NULL)
+    {
+        texture = SDL_CreateTextureFromSurface(renderer, loadedImage);
+        SDL_FreeSurface(loadedImage);
+    }
+    else
+    {
+        const char *err = SDL_GetError();
+        printf("%s\n", err);
+    }
+
+    return texture;
+}
+
+void ApplySurface(int x, int y, SDL_Texture *tex, SDL_Renderer *rend)
+{
+    SDL_Rect pos;
+    pos.x = x;
+    pos.y = y;
+
+    SDL_QueryTexture(tex, NULL, NULL, &pos.w, &pos.h);
+    SDL_RenderCopy(rend, tex, NULL, &pos);
+}
+
 
 int main(int argc, char **argv)
 {
-    printf("test\n");
-
     if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
     {
         const char *err = SDL_GetError();
@@ -12,53 +49,55 @@ int main(int argc, char **argv)
         return 1;
     }
 
-    SDL_Window *win = SDL_CreateWindow("Suck my ass!", 
-                                       100, 100, 640, 480,
-                                       SDL_WINDOW_SHOWN);
+    window = SDL_CreateWindow("Suck my ass!", SDL_WINDOWPOS_CENTERED,
+                              SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, 
+                              SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
 
-    if (win == NULL)
+    if (window == NULL)
     {
         const char *err = SDL_GetError();
         printf("SDL_CreateWindow Error: %s \n", err);
         return 1;
     }
 
-    SDL_Renderer *ren = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED |
-                                                    SDL_RENDERER_PRESENTVSYNC);
-    if (ren == NULL)
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED |
+                                           SDL_RENDERER_PRESENTVSYNC);
+    if (renderer == NULL)
     {
         const char *err = SDL_GetError();
         printf("SDL_CreateRenderer Error: %s \n", err);
         return 1;
     }
     
-    SDL_Surface *bmp = SDL_LoadBMP("pics/pis.bmp");
-    if (bmp == NULL)
-    {
-        const char *err = SDL_GetError();
-        printf("SDL_LoadBMP Error: %s \n", err);
-        return 1;
-    }
 
-    SDL_Texture *tex = SDL_CreateTextureFromSurface(ren, bmp);
-    SDL_FreeSurface(bmp);
+    SDL_Texture *background = NULL, *image = NULL;
+    background = LoadImage("pics/bck.bmp");
+    image = LoadImage("pics/pis.bmp");
 
-    if (tex == NULL)
-    {
-        const char *err = SDL_GetError();
-        printf("SDL_CreateTexture Error: %s \n", err);
-        return 1;
-    }
+    if (background == NULL || image == NULL)
+        return 2;
 
-    SDL_RenderClear(ren);
-    SDL_RenderCopy(ren, tex, NULL, NULL);
-    SDL_RenderPresent(ren);
+    SDL_RenderClear(renderer);
+    int bW, bH;
+    SDL_QueryTexture(background, NULL, NULL, &bW, &bH);
+    ApplySurface(0, 0, background, renderer);
+    ApplySurface(bW, 0, background, renderer);
+    ApplySurface(0, bH, background, renderer);
+    ApplySurface(bW, bH, background, renderer);
 
+    int iW, iH;
+    SDL_QueryTexture(image, NULL, NULL, &iW, &iH);
+    int x = SCREEN_WIDTH / 2 - iW / 2;
+    int y = SCREEN_HEIGHT / 2 - iH / 2;
+    ApplySurface(x, y, image, renderer);
+
+    SDL_RenderPresent(renderer);
     SDL_Delay(2000);
 
-    SDL_DestroyTexture(tex);
-    SDL_DestroyRenderer(ren);
-    SDL_DestroyWindow(win);
+    SDL_DestroyTexture(background);
+    SDL_DestroyTexture(image);
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
     SDL_Quit();
 
     return 0;
